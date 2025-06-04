028e6ebbcf1114055891a3bdcea4a0d1e3d7f0e4
DiannaaaMmrrr <murlaevadiana@gmail.com>
2025-06-04 22:20:15 +0300

    init project

---
+++ src/test/java/com/vk/log/bot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.log.bot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
